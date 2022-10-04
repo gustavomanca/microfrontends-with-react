4166ab9ea61dc63290c1bf941f6a44f59de5d775
Gustavo Manca <gustavomanca@ioasys.com.br>
2022-10-04 18:30:13 -0300

    add whatever message

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -2,6 +2,7 @@ import { mount as productsMount } from 'products/ProductsIndex'
 import { mount as cartMount } from 'cart/CartShow'
 
 console.log('Container...')
+console.log('whatever')
 
 productsMount(document.querySelector('#my-products'))
 cartMount(document.querySelector('#my-cart'))
