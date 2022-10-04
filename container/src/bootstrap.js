64f202d340eca5f72ecf1282bbb6e1086ae4949e
Gustavo Manca <gustavomanca@ioasys.com.br>
2022-10-04 18:31:18 -0300

    remove whatever message

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -2,7 +2,6 @@ import { mount as productsMount } from 'products/ProductsIndex'
 import { mount as cartMount } from 'cart/CartShow'
 
 console.log('Container...')
-console.log('whatever')
 
 productsMount(document.querySelector('#my-products'))
 cartMount(document.querySelector('#my-cart'))
